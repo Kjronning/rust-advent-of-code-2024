809f1264bcf4ddb6cd178bbe18807791b6275904
Joakim Rönning <kjronning@gmail.com>
2024-12-06 07:58:17 +0100

    day one done

---
+++ day-1/src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::convert::TryInto;
+mod reader;
+
+
+// gets the input, makes left and right column of sorted integers
+fn parse_input(lines: io::Lines<io::BufReader<File>>) -> (Vec<i32>, Vec<i32>) {
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+    for line in lines.flatten() {
+        let mut parts = line.split_whitespace();
+        left.push(parts.next().unwrap().parse().unwrap());
+        right.push(parts.next().unwrap().to_string().parse().unwrap());
+    }
+    left.sort();
+    right.sort();
+    return (left, right); 
+}
+
+fn main() {
+    let input = reader::read("./input.txt");
+    let (left, right) = parse_input(input);
+    
+    // Part One
+    // calculate distances
+    let distance: i32 = left.clone().into_iter().zip(right.clone().into_iter()).map(|(x, y)| (x-y).abs()).reduce(|acc, e| acc+e).unwrap();
+    
+    // print result
+    println!("Part One result: {}", distance);
+
+    //Part Two
+    let mut similarities: HashMap<i32, i32> = HashMap::new();
+
+    // get similarities
+    left.clone().into_iter().for_each(|n| { similarities.entry(n).or_insert(right.clone().into_iter().filter(|v: &i32| n == v.clone()).count().try_into().unwrap()); });
+
+    //reduce total
+    let similarity_score:i32 = similarities.into_iter().map(|(key, value)| key*value).reduce(|acc, e| acc + e).unwrap();
+
+    // print result
+    println!("Part Two result: {similarity_score}");
+
+}
+
---
+++ day-1/src/reader.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+pub fn read(filename: &str) -> std::io::Lines<io::BufReader<File>>{
+    // File hosts.txt must exist in the current path
+    return read_lines(filename).unwrap();
+}
+
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
